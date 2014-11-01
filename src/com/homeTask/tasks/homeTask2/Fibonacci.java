b8fe9fc41168900cacd6009b82f388fec23d59b4
Pandviny <vinnyk.igor@gmail.com>
2014-11-01 12:10:49 +0200

    Oridginal

---
+++ src/com/homeTask/tasks/homeTask2/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.homeTask.tasks.homeTask2;
+
+public class Fibonacci{
+    public static void main(String[]args){
+        int a,b=2,c;
+        for(a=1;a<2;a++)
+            System.out.print(a);
+        for(a=1;a<2;a++)
+            System.out.print(" "+a);
+        c=1;
+        a=1;
+        for(b=0;b<9;b++){
+            c=c+a;
+            System.out.print(" "+c);
+            a=c-a;
+        }
+
+    }
+}
