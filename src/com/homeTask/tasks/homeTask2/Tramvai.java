9f06b2035fab4768f5f7b51d84b193c96835bf54
Pandviny <vinnyk.igor@gmail.com>
2014-11-01 12:03:40 +0200

    Oridginal

---
+++ src/com/homeTask/tasks/homeTask2/Tramvai.java
@@ -0,0 +1,27 @@
+package com.homeTask.tasks.homeTask2;
+
+public class Tramvai {
+    public static void main(String[] args) {
+        byte a;
+        int bilet;
+        byte c;
+        int priz = 0;
+        for (bilet = 1; bilet < 1000000; bilet++) {
+            byte first3 = 0;
+            byte last3 = 0;
+            int nomerLast = bilet;
+            for (a = 0; a < 3; a++) {
+                last3 = (byte) (last3 + (nomerLast % 10));
+                nomerLast /= 10;
+            }
+            int nomerFirst = bilet / 1000;
+            for (c = 0; c < 3; c++) {
+                first3 = (byte) (first3 + (nomerFirst % 10));
+                nomerFirst /= 10;
+            }
+            if (last3 == first3) priz++;
+        }
+        System.out.println("Количество призов нужное для акции "+priz);
+
+    }
+}
