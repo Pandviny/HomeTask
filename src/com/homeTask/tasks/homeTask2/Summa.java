95725fac5c3de81e306766a1ed509bcc88db27b8
Pandviny <vinnyk.igor@gmail.com>
2014-11-01 12:37:59 +0200

    summa

---
+++ src/com/homeTask/tasks/homeTask2/Summa.java
@@ -0,0 +1,20 @@
+package com.homeTask.tasks.homeTask2;
+
+import java.util.Scanner;
+
+public class Summa{
+    public static void main(String[] args){
+        int a,c=0;
+        Scanner scn=new Scanner(System.in);
+        if(scn.hasNextInt()){
+            a=scn.nextInt();
+            while(a!=0){
+                c=c+a%10;
+                a/=10;
+            }
+
+            System.out.println("Сумма всех чисел введенного числа равна "+c);
+        }
+        else System.out.println("Ошибка. Вы ввели не число!");
+    }
+}
