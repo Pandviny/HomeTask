377a953412005ce864a9f981176ccf1b015c7eb5
Pandviny <vinnyk.igor@gmail.com>
2014-11-01 13:06:53 +0200

    ClockSumma

--- src/com/homeTask/tasks/homeTask2/ClockSumm.java
+++ src/com/homeTask/tasks/homeTask2/ClockSumm.java
@@ -3,7 +3,7 @@ package com.homeTask.tasks.homeTask2;
 public class ClockSumm {
     public static void main(String[] args){
         int b=0;
-        for(int h=1;h<=24;h++){
+        for(int h=1;h<=23;h++){
             for(int m=0;m<=59;m++){
                 if(h/10%10==m%10&h%10==m/10%10){
                     b++;
